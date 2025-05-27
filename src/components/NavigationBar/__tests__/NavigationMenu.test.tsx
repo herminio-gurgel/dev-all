bfac41ea72a8d6359359a32e6d4060addb13d9c0
Herminio Gurgel <herminio.gurgel@gmail.com>
2025-05-27 22:04:52 +0000

    fix: ajustando as props nos testes do `NavigationMenu`

--- src/components/NavigationBar/__tests__/NavigationMenu.test.tsx
+++ src/components/NavigationBar/__tests__/NavigationMenu.test.tsx
@@ -2,19 +2,26 @@ import { render } from "@testing-library/react";
 import NavigationMenu from "../partials/NavigationMenu";
 
 describe("NavigationMenu", () => {
+  const mockToggleMenu = jest.fn();
   it("renders without crashing", () => {
-    const { container } = render(<NavigationMenu menuActive={false} />);
+    const { container } = render(
+      <NavigationMenu menuActive={false} toggleMenu={mockToggleMenu} />,
+    );
     expect(container).toBeInTheDocument();
   });
 
   it("applies 'is-active' class when menuActive is true", () => {
-    const { container } = render(<NavigationMenu menuActive={true} />);
+    const { container } = render(
+      <NavigationMenu menuActive={true} toggleMenu={mockToggleMenu} />,
+    );
     const navbarMenu = container.querySelector(".navbar-menu");
     expect(navbarMenu).toHaveClass("is-active");
   });
 
   it("does not apply 'is-active' class when menuActive is false", () => {
-    const { container } = render(<NavigationMenu menuActive={false} />);
+    const { container } = render(
+      <NavigationMenu menuActive={false} toggleMenu={mockToggleMenu} />,
+    );
     const navbarMenu = container.querySelector(".navbar-menu");
     expect(navbarMenu).not.toHaveClass("is-active");
   });
