811973caccb6645fc35c0bf0e0ba612d14805c24
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-05-27 09:27:39 -0300

    test: testando o componente NavigationBrand
    - Interração de usuário
    - Aplicação dinâmica de classes

---
+++ src/components/NavigationBar/__tests__/NavigationBrand.test.tsx
@@ -0,0 +1,27 @@
+import NavigationBrand from "../partials/NavigationBrand";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+describe("NavigationBrand", () => {
+  const mockToogleMenu = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("toggles the menu when burger icon is clicked", async () => {
+    render(<NavigationBrand menuActive={false} toogleMenu={mockToogleMenu} />);
+    const burgerIcon = screen.getByRole("button", { name: "menu" });
+
+    await userEvent.click(burgerIcon);
+
+    expect(mockToogleMenu).toHaveBeenCalledTimes(1);
+  });
+
+  it("applies 'is-active' class when menu is active", () => {
+    render(<NavigationBrand menuActive={true} toogleMenu={mockToogleMenu} />);
+    const burgerIcon = screen.getByRole("button", { name: "menu" });
+
+    expect(burgerIcon).toHaveClass("is-active");
+  });
+});
