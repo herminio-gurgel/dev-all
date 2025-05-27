dac6e5134e721c98d20ae07f8ce6b0a935476406
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-05-27 11:38:32 -0300

    chore: ajustando o lint-staged por conta de conflitos com o tsc e arquivos fora da área de staging

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,4 @@ export default {
   "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
   "*.{html,css,yaml,md,json}": "prettier --write",
   "*.test.{js,jsx,ts,tsx}": "jest --findRelatedTests --passWithNoTests",
-  "**/*.ts?(x)": () => "tsc -p tsconfig.app.json --noEmit",
 };
