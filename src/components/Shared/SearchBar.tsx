1785df474117960e746317f05017dc6bca92db39
Herminio Gurgel <herminio.gurgel@gmail.com>
2025-06-03 20:53:04 +0000

    style(search bar):  mantendo o visual mais alinhado com o restante do projeto

--- src/components/Shared/SearchBar.tsx
+++ src/components/Shared/SearchBar.tsx
@@ -1,13 +1,28 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  background-color: white;
+  caret-color: hsl(0, 0%, 4%);
+  color: black;
+  &::placeholder {
+    color: black;
+    opacity: 0.5;
+  }
+`;
 
 const SearchBar = () => {
   return (
     <form className="is-display-flex">
       <div className="field">
-        <p className="control has-icons-left">
-          <input className="input" type="search" placeholder="Pesquisar" />
-          <i className="icon is-left">
+        <p className="control has-icons-left ">
+          <StyledInput
+            className="input is-text"
+            type="text"
+            placeholder="Pesquisar"
+          />
+          <i className="icon is-left has-text-text">
             <FontAwesomeIcon icon={faMagnifyingGlass} />
           </i>
         </p>
