bdfe63c34250daf5bf4f06b992e91926ce4101ba
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-06-03 12:08:46 -0300

    feat: implementando a requisição para carregar mais posts do servidor

--- src/components/PostList/partials/PostList.tsx
+++ src/components/PostList/partials/PostList.tsx
@@ -2,16 +2,27 @@ import { useEffect, useState } from "react";
 import { getPosts } from "../../../services/devallApi";
 import { Post } from "../../../types/Post";
 import PostComponent from "./PostComponent";
+import LoadMore from "./LoadMore";
 
 const PostList = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [page, setPage] = useState(0);
   const [loading, setLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
+    if (page === 0) return;
+
     const fetchPosts = async () => {
+      setLoading(true);
       try {
-        const data = await getPosts();
-        setPosts(data);
+        const data = await getPosts({ page });
+
+        if (data.length === 0) {
+          setHasMore(false);
+        } else {
+          setPosts((prevPosts) => [...prevPosts, ...data]);
+        }
       } catch (error) {
         console.error("Erro ao buscar posts:", error);
       } finally {
@@ -20,20 +31,31 @@ const PostList = () => {
     };
 
     void fetchPosts();
+  }, [page]);
+
+  useEffect(() => {
+    setPage(1);
   }, []);
+
+  const handleLoadMore = () => {
+    setLoading(true);
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <>
-      {loading
+      {page === 1 && loading
         ? [...Array<Post>(20)].map((_, i) => (
             <PostComponent key={i} isLoading={true} />
           ))
         : posts.map((post) => (
             <PostComponent key={post.id} post={post} isLoading={false} />
           ))}
-      <div className="mt-2">
-        <button className="button is-light">Carregar mais</button>
-      </div>
+      <LoadMore
+        isLoading={loading}
+        hasMore={hasMore}
+        onClick={handleLoadMore}
+      />
     </>
   );
 };
