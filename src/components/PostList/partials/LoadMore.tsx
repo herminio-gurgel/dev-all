557d4cb22218ac515637b3f64252bd88362b922d
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-06-05 09:51:41 -0300

    feat: exibir mensagem quando não houver mais posts para carregar

--- src/components/PostList/partials/LoadMore.tsx
+++ src/components/PostList/partials/LoadMore.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPersonDigging } from "@fortawesome/free-solid-svg-icons/faPersonDigging";
+
 interface LoadMoreProps {
   isLoading: boolean;
   hasMore: boolean;
@@ -17,8 +20,19 @@ const LoadMore = ({ isLoading, hasMore, onClick }: LoadMoreProps) => {
       >
         Carregar mais
       </button>
+      <article className={`${hasMore ? "is-hidden" : ""} message`}>
+        <div
+          className="message-body is-display-flex"
+          style={{ maxWidth: "765px" }}
+        >
+          <FontAwesomeIcon
+            className="has-text-centered image is-32x32 pr-5"
+            icon={faPersonDigging}
+          />
+          <p>Você chegou ao fundo do post, não há mais dados para carregar.</p>
+        </div>
+      </article>
     </div>
   );
 };
-
 export default LoadMore;
