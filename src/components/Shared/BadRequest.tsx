bdeb8998826758ed2c51c58744491f722f9066c5
Hermíno Carlos Gurgel Medeiros Netto <herminio.gurgel@gmail.com>
2025-06-09 13:03:36 -0300

    style: ajustando a mensagem com título e link para contato

--- src/components/Shared/BadRequest.tsx
+++ src/components/Shared/BadRequest.tsx
@@ -5,14 +5,22 @@ interface BadRequestProps {
 const BadRequest = ({ error }: BadRequestProps) => {
   if (error) {
     return (
-      <article className="message is-danger">
+      <article
+        className="message is-danger"
+        style={{ maxWidth: "fit-content" }}
+      >
+        <div className="message-header">Erro</div>
         <div className="message-body">
-          Desculpe, não conseguimos encontrar o que você está procurando.
+          <p>Desculpe, não conseguimos encontrar o que você está procurando.</p>
+          <p>
+            Tente novamente mais tarde ou{" "}
+            <a href=" mailto:contato@itexto.com.br">entre em contato</a>
+          </p>
         </div>
       </article>
     );
   }
 
-  return <></>;
+  return null;
 };
 export default BadRequest;
